278c42ffe72a4bb4c4c6b1736e3910609c614184
Shreya Nigam <shreya.nigam1062@gmail.com>
2024-11-18 16:51:44 +0530

    cloud db connection

--- server/database/db.js
+++ server/database/db.js
@@ -1,10 +1,19 @@
 const Pool = require("pg").Pool;
 
+// DEV MODE
+// const pool = new Pool({
+//   user: process.env.DATABASE_USER,
+//   password: process.env.DATABASE_PASSWORD,
+//   port: process.env.DATABASE_PORT,
+//   database: process.env.DATABASE_NAME,
+// });
+
+// PRODUCTION MODE
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT,
-  database: process.env.DATABASE_NAME,
+  connectionString: process.env.DATABASE_URL, // Use connectionString instead of separate keys
+  ssl: {
+    rejectUnauthorized: false, // Required for connecting to Render's managed PostgreSQL
+  },
 });
 
 module.exports = pool;
